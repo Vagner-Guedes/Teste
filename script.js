0fadf0ee9e21695307e09191a06c79533b4246a1
Vagner Guedes <101615151+Vagner-Guedes@users.noreply.github.com>
2024-01-25 13:36:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function retornoBlock() {
     try {
-      parent.NeonewsInterface.callBlock('A251C3A3-577A-4E37-A34E-1C0E940C9726');
+      parent.parent.NeonewsInterface.callBlock('A251C3A3-577A-4E37-A34E-1C0E940C9726');
               
 
         alert('você será redirecionado');
