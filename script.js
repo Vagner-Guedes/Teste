11b0364e5d3e6662c7839d75f32d0b876de95a6d
Vagner Guedes <101615151+Vagner-Guedes@users.noreply.github.com>
2024-01-25 12:24:41 -0300

    Add files

---
+++ script.js
@@ -0,0 +1,16 @@
+function retornoBlock() {
+    try {
+        //NeonewsInterface.callBack('A251C3A3-577A-4E37-A34E-1C0E940C9726');
+       alert('você será redirecionado');
+    } catch (err) {
+        alert(err);
+    }
+}
+
+
+
+
+
+
+
+
